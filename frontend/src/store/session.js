84df3a412b893407fd6b20f2dfbc332b692cc185
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-04-24 17:54:51 -0400

    Added user restore thunk to maintain session

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,17 +3,25 @@ import { csrfFetch } from "./csrf";
 const ADD_USER = "session/ADD_USER";
 const REMOVE_USER = "session/REMOVE_USER";
 
-const addUser = (user) => {
+export const addUser = (user) => {
   return {
     type: ADD_USER,
     user,
   };
 };
 
-const removeUser = () => {
+export const removeUser = () => {
   return {
     type: REMOVE_USER,
   };
+};
+
+export const userRestore = () => async (dispatch) => {
+  const res = await csrfFetch("/api/session");
+  const { user } = await res.json();
+
+  dispatch(addUser(user));
+  return user;
 };
 
 export const userLogin = ({ credential, password }) => async (dispatch) => {
@@ -38,6 +46,11 @@ const sessionReducer = (state = { user: null }, action) => {
       return {
         ...state,
         user: action.user,
+      };
+    case REMOVE_USER:
+      return {
+        ...state,
+        user: null,
       };
     default:
       return state;
