c4bfb7bd5675a01d4651b6fdc17acc33972ba939
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-04-25 15:17:47 -0400

    Added login modal with styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 import App from "./App";
 import configureStore from "./store";
 import * as sessionActions from "./store/session";
+import ModalProvider from "./context/Modal.js";
 
 const store = configureStore();
 
@@ -21,9 +22,11 @@ if (process.env.NODE_ENV !== "production") {
 function Root() {
   return (
     <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </ReduxProvider>
   );
 }
