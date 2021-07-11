c9acdf52f18ce2fd357fe6f880691f7b1006898d
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-07-11 18:18:11 -0400

    Styled banner and changed photo

--- frontend/src/components/MainPageHeader/index.js
+++ frontend/src/components/MainPageHeader/index.js
@@ -1,10 +1,17 @@
-import ezBanner from "../../img/ez-banner.png";
+import ezBanner from "../../img/ezvent-banner.jpg";
 import "./MainPageHeader.css";
 
 const MainPageHeader = () => {
   return (
-    <div>
+    <div className="banner-container">
+      <div className="text-container">
+        <span className="span-discover">Discover the best</span>
+        <span className="span-events">events</span>
+        <button className="browse-btn">Browse events</button>
+        </div>
+      <div className="img-container">
       <img className="main-page-header" src={ezBanner}></img>
+      </div>
     </div>
   );
 };
