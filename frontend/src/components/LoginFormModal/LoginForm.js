41d320deb01ea0063fe6f9f89f740a8eec7983fc
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-07-11 20:11:14 -0400

    Fixed login to signup bug and added overall styling

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, NavLink, useHistory } from "react-router-dom";
 import "./LoginForm.css";
 
-function LoginForm() {
+function LoginForm({setShowModal}) {
   const dispatch = useDispatch();
+  const history = useHistory();
   //   const sessionUser = useSelector((state) => state.session.user);
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
@@ -26,6 +27,11 @@ function LoginForm() {
 
   const handleDemoLogin = () => {
     return dispatch(sessionActions.userLogin({ credential: "demouser", password: "password" }))
+  }
+
+  const toSignUp = () => {
+    history.push("/signup")
+    setShowModal(false)
   }
 
   return (
@@ -62,6 +68,7 @@ function LoginForm() {
         <button id="login-btn" type="submit">
           Log In
         </button>
+        <div className="account-signup-text">Don't have an account? <span onClick={toSignUp} className="signup-link">Sign up</span></div>
         <div className="demo-container">Or continue as a <span onClick={handleDemoLogin} className="span-demo">demo user</span>.</div>
       </form>
     </div>
