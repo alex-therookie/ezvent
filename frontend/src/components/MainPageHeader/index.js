6025b569e671940b6cb753099847977304cb7e87
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-07-12 23:54:20 -0400

    Added link to browse events

--- frontend/src/components/MainPageHeader/index.js
+++ frontend/src/components/MainPageHeader/index.js
@@ -1,13 +1,18 @@
+import {useHistory} from "react-router-dom"
 import ezBanner from "../../img/ezvent-banner.jpg";
 import "./MainPageHeader.css";
 
 const MainPageHeader = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/events")
+  }
   return (
     <div className="banner-container">
       <div className="text-container">
         <span className="span-discover">Discover the best</span>
         <span className="span-events">events</span>
-        <button className="browse-btn">Browse events</button>
+        <button className="browse-btn" onClick={handleClick}>Browse events</button>
         </div>
       <div className="img-container">
       <img className="main-page-header" src={ezBanner}></img>
